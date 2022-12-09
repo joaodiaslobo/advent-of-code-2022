6a10b61352aeddc045b2819b1af36765a0abdaca
João Lobo <joaodlobo@gmail.com>
2022-12-09 03:46:40 +0000

    Structure update

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_all_days():
+    folder = "./"
+    folders = [name for name in os.listdir(folder) if
+               os.path.isdir(os.path.join(folder, name)) and name.startswith("day")]
+    last = folders[-1:]
+    print(last)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    get_all_days()
